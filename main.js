c07ef949d12d6fa7cf6cb77181950da6373916f2
rishik006 <97524363+rishik006@users.noreply.github.com>
2023-05-08 07:32:15 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+noseX=0
+noseY=0
+
+function preload(){
+    specs=loadImage('https://i.postimg.cc/NfsvGsnK/glasses-PNG54277.png')
+}
+
+function setup(){
+    canvas = createCanvas(300,300)
+    canvas.center()
+    video = createCapture(VIDEO)
+    video.size(300,300)
+    video.hide()
+    poseNet = ml5.poseNet(video, modelLoaded())
+    poseNet.on('pose', gotPoses)
+}
+
+function modelLoaded(){
+    console.log('PoseNet Is Intialized ')
+}
+
+function gotPoses(results) { if(results.length > 0) { console.log(results); noseX = results[0].pose.rightEye.x; noseY = results[0].pose.rightEye.y; } }
+
+function draw(){
+    image(video, 0, 0, 300, 300)
+    image(specs,noseX,noseY,110,30)
+}
+
+function take_snapshot(){
+    save('filter.png')
+}
